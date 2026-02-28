7b2032c16ffbc0747a5d1bac1c3fa88ef3622600
redlatrix <mariacamila0103@hotmail.com>
2026-02-28 11:31:52 -0500

    feat(Inventory): models estante y libro

--- introducion/inventory/models.py
+++ introducion/inventory/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Estante(models.Model):
+    nombre = models.CharField(max_length=100)
+    piso = models.CharField(max_length=50, help_text="Ej: Pasillo A, Planta 2")
+
+    def __str__(self):
+        return f"{self.nombre} ({self.piso})"
+
+
+class Libro(models.Model):
+    titulo = models.CharField(max_length=200)
+    autor = models.CharField(max_length=100)
+    año = models.DateField()
+    stock = models.IntegerField(default=0)
+    
+    estante = models.ForeignKey(Estante, on_delete=models.SET_NULL, null=True, blank=True, related_name='libros')
+
+    def __str__(self):
+        return self.titulo
