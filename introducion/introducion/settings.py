83ca0675f690ac27e84f8063c25c1e04a65c9276
Jorge Andrés Amaya Bermudez <tics@lan.quifarma.com>
2026-03-21 14:36:37 -0500

    borre comentarios

--- introducion/introducion/settings.py
+++ introducion/introducion/settings.py
@@ -114,8 +114,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'introducion.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+
 
 DATABASES = {
     'default': {
